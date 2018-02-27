bf1d887a2af20b09bca79c3606d8a59c3c840277
Frank H Mcmanus <frank@thefrankharvey.com>
2018-02-27 23:19:24 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
 def prime?(n)
-  if n % 2 == 0
+  if n > 2 && n % 2 == 0
     puts false
-  elsif n % 3 == 0
+  elsif n > 3 && n % 3 == 0
     puts false
-  elsif n % 5 == 0
+  elsif n > 5 && n % 5 == 0
     puts false
-  elsif n % 7 == 0
+  elsif n > 7 && n % 7 == 0
     puts false
-  elsif n % 11 == 0
+  elsif n > 11 && n % 11 == 0
     puts false
   else
     puts true
