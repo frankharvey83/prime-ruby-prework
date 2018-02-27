e54f78ec3877dbd3e4bd098581b4084d7b34b126
Frank H Mcmanus <frank@thefrankharvey.com>
2018-02-27 23:16:25 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(n)
+  if n % 2 == 0
+    puts false
+  elsif n % 3 == 0
+    puts false
+  elsif n % 5 == 0
+    puts false
+  elsif n % 7 == 0
+    puts false
+  elsif n % 11 == 0
+    puts false
+  else
+    puts true
+  end
+end
