1df45ea9404bde86feefda0d5bb59f077d689909
Frank H Mcmanus <frank@thefrankharvey.com>
2018-02-28 01:03:29 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,15 @@
 def prime?(n)
+if n < 2
+  false
+else
 arr = (2..Math.sqrt(n).floor).to_a
 arr.each do |x|
-if n % x == 0
+if n % x == 0 && n != 2
   false
+elsif n % x == 0 && n !=3
+  false
+else
+  true
+    end
   end
-end
 end
