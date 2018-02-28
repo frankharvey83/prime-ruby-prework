880bbbc2127d0c46a471d4b6ea0dd1297cad8f47
Frank H Mcmanus <frank@thefrankharvey.com>
2018-02-28 00:28:32 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(n)
 arr = (2..Math.sqrt(n).floor).to_a
-arr.each { |x| n / x }
 if n < 2
   false
-elsif n > 3 && n % x ==0
+arr.each { |x| n / x }
+elsif n > 3 && n % x == 0
   false
 else
   true
